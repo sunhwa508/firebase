129370c80edbe4da6e1e9b0ac990b81543afa487
Jamie <2017sunhwa@gmail.com>
2023-03-29 08:02:45 +0900

    fix: router 설정 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,12 @@
-import { useEffect, useRef, useState } from "react";
-import "./App.css";
-import { db } from "./firebase-config";
-import {
-  collection,
-  getDocs,
-  addDoc,
-  updateDoc,
-  deleteDoc,
-  doc,
-} from "firebase/firestore";
+
+import "./pages/App.css";
+import Router from "./Router";
+
 function App() {
-  const [newName, setNewName] = useState("");
-  const [newTeam, setNewTeam] = useState("");
-
-  const [menus, setMenus] = useState<
-    { id: string; name?: string; team?: string }[]
-  >([]);
-  const usersCollectionRef = collection(db, "menus");
-
-  const createUser = async () => {
-    await addDoc(usersCollectionRef, { name: newName, team: newTeam });
-  };
-
-  const deleteUser = async (id: string) => {
-    const userDoc = doc(db, "menus", id);
-    await deleteDoc(userDoc);
-  };
-
-  const updateUser = async (id: string, team?: string) => {
-    const userDoc = doc(db, "menus", id);
-    const newFields = { team: "Mobile" };
-    await updateDoc(userDoc, newFields);
-  };
-
-  useEffect(() => {
-    const getUsers = async () => {
-      const data = await getDocs(usersCollectionRef);
-      setMenus(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    };
-
-    getUsers();
-  }, [createUser]);
-
   return (
-    <div className='App'>
-      <input
-        placeholder='Name...'
-        onChange={(event) => {
-          setNewName(event.target.value);
-        }}
-      />
-      <input
-        placeholder='team...'
-        onChange={(event) => {
-          setNewTeam(event.target.value);
-        }}
-      />
-      <button onClick={createUser}>Create User</button>
-      {menus.map((menu) => {
-        return (
-          <div>
-            <h1>Name: {menu.name}</h1>
-            <h1>Name: {menu.team}</h1>
-            <button onClick={() => updateUser(menu.id, menu.team)}>
-              Change Team
-            </button>
-            <button onClick={() => deleteUser(menu.id)}>delete Team</button>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <Router />
+    </>
   );
 }
 
